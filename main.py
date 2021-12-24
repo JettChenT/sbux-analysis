47a291b8110d08023a00a45cea8a0424b51712d9
JettChenT <jettchen12345@gmail.com>
2021-12-24 15:48:47 +0800

    add insights

--- main.py
+++ main.py
@@ -5,12 +5,46 @@ import requests
 import streamlit as st
 import pydeck as pdk
 import h3
+import plotly.express as px
+import plotly.graph_objects as go
 #%%
 st.title("Starbucks!")
 st.write("## The financial report (data was gathered from [FAPI](https://site.financialmodelingprep.com/financial-statements/SBUX))")
-data = pd.read_csv("./sbux.csv")
-data.drop(columns=data.columns[0], axis=1, inplace=True)
+data = pd.read_csv('./finance_data.csv')
+data.set_index('calendarYear')
+data = data.iloc[::-1]
+
 st.write(data)
+def bplot(col, desc=''):
+    return px.bar(data, x='calendarYear', y=col, color=col, color_continuous_scale=px.colors.sequential.Blugrn, title=desc if desc else col)
+
+for col,dsc in zip(['revenue', 'netIncome', 'grossProfit'], ['Starbucks Revenue By Year','Net Income By Year', 'Gross Profit By Year']):
+    st.plotly_chart(bplot(col, dsc))
+
+# year = st.selectbox('Year number: ', [2021])
+year = 2021
+
+rr = 'relative'
+tt = 'total'
+
+# st.write(year)
+
+fig = go.Figure(
+    go.Waterfall(
+        name = '20' , orientation='v',
+        measure = [rr,rr,rr,tt,rr,rr,tt,rr,tt],
+        y=[18.32*1e9, 5.05*1e9, 5.69*1e9,None, data['interestIncome'][0], -data['costAndExpenses'][0], None, -data['incomeTaxExpense'][0], None],
+        # y=[10,-2,8],
+        x=['Beverage revenues', 'Food revenues', 'Other revenues', 'Gross revenue', 'Interest income', 'Costs & Expenses', 'Income before tax', 'tax', 'net income']
+    )
+)
+
+
+fig.update_layout(
+    title = f"Starbucks financial statement waterfall chart: year {year}",
+    showlegend = True
+)
+st.write(fig)
 
 #%%
 st.write("## A map of all Starbucks stores (data from [Kaggle](https://www.kaggle.com/starbucks/store-locations/version/1))")
